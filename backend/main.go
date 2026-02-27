ff523aa3bb84908a404bd7d2391138786beb9406
sufiyanshaikh01 <softwaremastar0010@gmail.com>
2026-02-27 11:56:29 +0530

    Add new Fature

--- backend/main.go
+++ backend/main.go
@@ -96,7 +96,7 @@ func deleteDonor(phone string) error {
 func donorHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	if r.Method == "OPTIONS" {
